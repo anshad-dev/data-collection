c7a446596b526884d126f8d6f8998a6bd025b9e8
anshad-dev <anshad@iocod.com>
2025-04-28 09:40:00 +0530

    Enhance lender update functionality to support case-insensitive name matching

--- internal/repositories/lenders_repo.go
+++ internal/repositories/lenders_repo.go
@@ -27,7 +27,13 @@ func (r *lendersRepo) UpdateByName(ctx context.Context, lenderName string, updat
 		"$set": updateData,
 	}
 	opts := options.Update().SetUpsert(false) // avoid inserting new document if not found
-	result, err := r.col.UpdateOne(ctx, bson.M{"lender_name": lenderName}, update, opts)
+	filter := bson.M{
+		"lender_name": bson.M{
+			"$regex":   lenderName,
+			"$options": "i", // Case-insensitive option
+		},
+	}
+	result, err := r.col.UpdateOne(ctx, filter, update, opts)
 	if err != nil {
 		return nil, err
 	}
